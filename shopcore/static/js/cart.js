ac4bc13c59fef0095f83e2c67067b1c163cdbb74
Serebrennikov Sergei <smit7bb@mail.ru>
2020-12-05 19:48:55 +0500

    Написал функцию для отправки данных при нажатии кнопки добавить на главной странице Отправляет id продукта и action: add

--- shopcore/static/js/cart.js
+++ shopcore/static/js/cart.js
@@ -1,6 +1,5 @@
 const updateBtn = document.querySelectorAll('.update-cart')
 
-
 // //рабочий метод можно использовать его
 // // i += 1 можно заменить на i++
 // for (let i = 0; i < updateBtn.length; i += 1) {
@@ -8,27 +7,54 @@ const updateBtn = document.querySelectorAll('.update-cart')
 //         let productId = this.dataset.product
 //         let action = this.dataset.action
 //         console.log('productId:', productId, 'Action:', action)
+//         authorizationCheck(user, productId, action)
 //     })
 // }
+
 function checkButtons() {
     for (let btn of updateBtn) {
-    btn.addEventListener('click', function () {
-        let productId = this.dataset.product
-        let action = this.dataset.action
-        console.log('productId:', productId, 'Action:', action)
+        btn.addEventListener('click', function () {
+            let productId = this.dataset.product
+            let action = this.dataset.action
+            console.log('productId:', productId, 'Action:', action)
 
-        authorizationCheck(user)
-    });
-}}
+            authorizationCheck(user, productId, action)
+        });
+    }
+}
 
-
-const authorizationCheck = (dataUser) => {
+const authorizationCheck = (dataUser, productId, action) => {
     console.log('USER:', dataUser)
     if (dataUser === "AnonymousUser") {
         console.log('Пользователь не авторизован')
     } else {
-        console.log('Пользователь авторизовался')
+        updateUserOrder(productId, action)
     }
+}
+
+function updateUserOrder(productId, action) {
+    //отправка данный на вьюху update_item
+    console.log('Пользователь авторизовался')
+
+    let url = 'update-item/'
+    let jsonString = JSON.stringify({'productId': productId, 'action': action})
+
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "X-CSRFToken": csrfToken,
+        },
+        body: jsonString,
+    })
+        .then((response) => {
+            return response.json()
+        })
+
+        .then((data) => {
+            console.log('data:', data)
+        })
 }
 
 checkButtons()
