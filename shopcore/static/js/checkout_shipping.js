d8d9827fa12075c74dcb0c774de03c69f5612bde
Serebrennikov Sergei <smit7bb@mail.ru>
2020-12-09 22:03:28 +0500

    Добавил обработчик который отслеживает нажатие кнопки continue, при нажатии скрывает ее и открывает доступ к вариантам оплаты. Второй обработчик отслеживает нажатие кнопки оплата.

--- shopcore/static/js/checkout_shipping.js
+++ shopcore/static/js/checkout_shipping.js
@@ -3,3 +3,20 @@ if(shipping === "False") {
     document.querySelector('#shipping-info')
         .innerHTML = ''
 }
+
+let form = document.querySelector('#form')
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    document.querySelector('#form-button').classList.add('hidden')
+    document.querySelector('#payment-info').classList.remove('hidden')
+
+})
+
+document.querySelector('#make-payment')
+    .addEventListener('click', function (e) {
+        submitFormData()
+    })
+
+function submitFormData() {
+    console.log('Кликнута кнопка оплаты')
+}
